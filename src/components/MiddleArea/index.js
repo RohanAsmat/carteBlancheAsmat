e86806cd252cd19a6f96ed090a73e3f5c26a461e
Rohan Asmat <m.rohan.a.asmat@gmail.com>
2019-09-17 15:47:52 +0200

    middle area updated

--- src/components/MiddleArea/index.js
+++ src/components/MiddleArea/index.js
@@ -2,33 +2,36 @@ import React from 'react';
 import SearchBar from '../../components/SearchBar';
 import CategoryItem from '../../components/CategoryItem';
 
-class MiddleArea extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-    };
-  }
+const MiddleArea = props => {
+  const style = {
+    main:{
+      borderRight: "1px solid #e5e8ec",
+      minHeight: "90vh",
+      width: '433px'
+    }
+  };
 
-  render () {
-    const style = {
-      main:{
-        borderRight: "1px solid #e5e8ec",
-        minHeight: "90vh",
-        width: '433px'
-      }
-    };
+  return(
+    <div style={style.main}>
+      <SearchBar cartItems={props.searchCategories }
+                 activeCell={props.activeCell}
+                 callbackFromParentForSearch={props.searchCallback} />
 
-    return(
-      <div style={style.main}>
-        <SearchBar cartItems={this.props.searchCategories } activeCell={this.props.activeCell} callbackFromParentForSearch={this.props.searchCallback} />
-
-        {this.props.activeCategories || this.props.categories.map(CATEGORIES => (CATEGORIES.name === this.props.activeCell ? (<CategoryItem key={'categoryItems'+CATEGORIES.name} items={CATEGORIES.item} callbackFromParent={this.props.callbackFromParent} id="1" />) : ('') ) )}
-        {this.props.activeCategories === true ? ( <CategoryItem key={'categoryItemsSearch'+this.props.activeCell+this.props.activeCategories} items={this.props.searchCategories } callbackFromParent={this.props.callbackFromParent} id="2" />) : ('')}
-
-      </div>
-    )
-  }
+      {props.activeCategories || props.categories.map(CATEGORIES =>
+        (CATEGORIES.name === props.activeCell ?
+          (<CategoryItem key={'categoryItems'+CATEGORIES.name}
+                         items={CATEGORIES.item}
+                         callbackFromParent={props.callbackFromParent} />)
+          : ('') ) )}
+      {props.activeCategories === true ?
+        (<CategoryItem
+          key={'categoryItemsSearch'+props.activeCell+props.activeCategories}
+          items={props.searchCategories }
+          callbackFromParent={props.callbackFromParent} />)
+        : ('')}
+    </div>
+  )
 }
 
 export default MiddleArea;
