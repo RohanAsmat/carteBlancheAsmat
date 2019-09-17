c507ca994f46e91f15e6110e8cdff18006f7774e
Rohan Asmat <m.rohan.a.asmat@gmail.com>
2019-09-17 16:17:37 +0200

    Updates in Home Page and App.js and deleting image

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import Navbar from './components/Navbar';
-import Drawer from 'material-ui/Drawer';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      drawerOpen: false
-    };
-  }
 
-  handleDrawerToggle = () => {
-    this.setState({
-      drawerOpen: !this.state.drawerOpen
-    });
-  };
   render() {
     const contentStyle = {
       transition: 'margin-left 450ms cubic-bezier(0.23, 1, 0.32, 1)'
     };
-    if (this.state.drawerOpen) {
-      contentStyle.marginLeft = 256;
-    }
 
     return (
       <div style={contentStyle}>
-        <Navbar handleDrawer={this.handleDrawerToggle} />
-        <Drawer
-          type="persistent"
-          onRequestChange={this.handleDrawerToggle}
-          open={this.state.drawerOpen}>
-          <h1>Stuff!</h1>
-        </Drawer>
+        <Navbar />
         <Switch>
           <Route path="/" component={Home} />
         </Switch>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,12 +5,12 @@ import RightArea from '../../components/RightArea';
 import MiddleArea from '../../components/MiddleArea';
 import {CATEGORIES} from '../../data/categories';
 
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      activeCell: 'BAR BQ',
+      activeCell: CATEGORIES[0].name,
       categories: CATEGORIES,
       cartItems: [],
       searchCategories: CATEGORIES[0].item,
@@ -19,97 +19,98 @@ class Home extends React.Component {
     };
   }
 
-  leftAreaCallback = (cellNameFromChild) => {
-      console.log('parent', cellNameFromChild )
-
-      var tempSearchCategories = this.state.searchCategories
-
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.activeCell !== this.state.activeCell)
+    {
+      var tempSearchCategories = []
       for( var i = 0 ; i < this.state.categories.length ; i++ )
-        if( cellNameFromChild === this.state.categories[i].name )
+        if( this.state.activeCell === this.state.categories[i].name )
           tempSearchCategories = this.state.categories[i].item
 
-      this.setState({activeCell: cellNameFromChild, searchCategories: tempSearchCategories, activeCategories:false})
+      this.setState({searchCategories: tempSearchCategories})
+    }
+  }
 
+  leftAreaCallback = (cellNameFromChild) => {
+    // updating list for search categories and active cell
+    var tempSearchCategories = this.state.searchCategories
+
+    for( var i = 0 ; i < this.state.categories.length ; i++ )
+      if( cellNameFromChild === this.state.categories[i].name )
+        tempSearchCategories = this.state.categories[i].item
+
+    this.setState({activeCell: cellNameFromChild,
+                   searchCategories: tempSearchCategories,
+                   activeCategories:false})
   }
 
   middleAreaCallback = (cellNameFromChild, price, quantity, serving) => {
-      console.log('parent', cellNameFromChild, price, quantity, serving, this.state.activeCell);
-      var tempCategoriesState = this.state.categories;
+    // updating quantity in categories and categories
+    var tempCategoriesState = this.state.categories;
 
-      for( var i = 0 ; i < this.state.categories.length ; i++ )
-        if( this.state.activeCell === this.state.categories[i].name )
-          for( var j = 0 ; j < this.state.categories[i].item.length ; j++)
-            if( this.state.categories[i].item[j].name === cellNameFromChild )
-              tempCategoriesState[i].item[j].quantity = quantity;
-              console.log("items", j);
-      this.setState({categories: tempCategoriesState});
+    for( var i = 0 ; i < this.state.categories.length ; i++ )
+      if( this.state.activeCell === this.state.categories[i].name )
+        for( var j = 0 ; j < this.state.categories[i].item.length ; j++)
+          if( this.state.categories[i].item[j].name === cellNameFromChild )
+            tempCategoriesState[i].item[j].quantity = quantity;
 
-      var tempCartItems = this.state.cartItems;
-      var boolItemInCart = false;
-      for( var x = 0 ; x < tempCartItems.length ; x++)
-        if( tempCartItems[x].name === cellNameFromChild )
-        {
-          boolItemInCart = true;
-          tempCartItems[x].quantity = quantity
-        }
+    this.setState({categories: tempCategoriesState});
 
-      if( boolItemInCart === false )
-        tempCartItems.push({'name':cellNameFromChild, 'price': price, 'quantity':quantity, 'serving':serving})
+    // updating quantity in cart items and a whole row if item not present
+    var tempCartItems = this.state.cartItems;
+    var boolItemInCart = false;
 
-        var totalAmount = 0
-        for( var z =0; z < this.state.cartItems.length; z++ )
-          totalAmount = totalAmount + (this.state.cartItems[z].quantity * this.state.cartItems[z].price)
-
-        console.log("total", this.state.cartItems);
-        this.setState({total: Number(totalAmount).toFixed(2)})
-
-
-      console.log(tempCartItems);
-
-  }
-  componentDidUpdate(prevProps, prevState) {
-      // only update chart if the data has changed
-      if (prevState.activeCell !== this.state.activeCell) {
-        var tempSearchCategories = []
-
-        for( var i = 0 ; i < this.state.categories.length ; i++ )
-          if( this.state.activeCell === this.state.categories[i].name )
-            tempSearchCategories = this.state.categories[i].item
-
-        this.setState({searchCategories: tempSearchCategories})
-
+    for( var x = 0 ; x < tempCartItems.length ; x++)
+      if( tempCartItems[x].name === cellNameFromChild )
+      {
+        boolItemInCart = true;
+        tempCartItems[x].quantity = quantity
       }
-    }
+
+    if( boolItemInCart === false )
+      tempCartItems.push({'name':cellNameFromChild,
+                          'price': price,
+                          'quantity':quantity,
+                          'serving':serving})
+
+    // updating state of total amount
+    var totalAmount = 0
+    for( var z =0; z < this.state.cartItems.length; z++ )
+      totalAmount = totalAmount
+        + (this.state.cartItems[z].quantity * this.state.cartItems[z].price)
+
+    this.setState({total: Number(totalAmount).toFixed(2)})
+  }
 
   searchCallback = (updatedList, keyss, activeCell) => {
     if( activeCell === this.state.activeCell)
     {
-
       if( keyss === '')
       {
+        // updating categories
         var tempSearchCategories = this.state.searchCategories
-
         for( var i = 0 ; i < this.state.categories.length ; i++ )
           if( activeCell === this.state.categories[i].name )
             tempSearchCategories = this.state.categories[i].item
 
-            this.setState({activeCategories: false, searchCategories: tempSearchCategories})
+            this.setState({activeCategories: false,
+                           searchCategories: tempSearchCategories})
       }
-
+      // resetting the categories list
       if( keyss !== '')
         this.setState({activeCategories: true, searchCategories: updatedList})
     }
-
   }
 
   rightAreaCallback = (nameFromChild, quantity) => {
+    // updating quantity in categories(&categories) list and categories
     var tempCategoriesState = this.state.categories;
-
     for( var i = 0 ; i < this.state.categories.length ; i++ )
         for( var j = 0 ; j < this.state.categories[i].item.length ; j++)
           if( this.state.categories[i].item[j].name === nameFromChild )
             tempCategoriesState[i].item[j].quantity = quantity;
 
+    // Case for updating quantity(in cartItems)  and total amount state
     var tempCartItems = this.state.cartItems
     var totalAmount = 0
     for( var x = 0 ; x < this.state.cartItems.length ; x++)
@@ -118,34 +119,42 @@ class Home extends React.Component {
       {
         totalAmount = totalAmount + ( quantity * this.state.cartItems[x].price)
         tempCartItems[x].quantity = quantity
-
       }else
-        totalAmount = totalAmount + (this.state.cartItems[x].quantity * this.state.cartItems[x].price)
-
+        totalAmount = totalAmount
+          + (this.state.cartItems[x].quantity * this.state.cartItems[x].price)
     }
 
+    // Case for Removing from Cart Items
     var tempCartForRemove = tempCartItems
     for( var k = 0 ; k < tempCartItems.length ; k++ )
       if( tempCartItems[k].quantity === 0)
         tempCartForRemove.splice(k,1)
 
     this.setState({categories: tempCategoriesState, cartItems: tempCartForRemove, total: Number(totalAmount).toFixed(2)});
-
   }
-
-
+  
   render() {
     return(
       <div>
         <Grid container spacing={0}>
           <Grid item>
-            <LeftArea callbackFromParent={this.leftAreaCallback} activeCell={this.state.activeCell}  categories={this.state.categories} />
+            <LeftArea callbackFromParent={this.leftAreaCallback}
+                      activeCell={this.state.activeCell}
+                      categories={this.state.categories} />
           </Grid>
           <Grid item>
-            <MiddleArea callbackFromParent={this.middleAreaCallback}  searchCallback={this.searchCallback} activeCell={this.state.activeCell} categories={this.state.categories} searchCategories={this.state.searchCategories} activeCategories={this.state.activeCategories} />
+            <MiddleArea callbackFromParent={this.middleAreaCallback}
+                        searchCallback={this.searchCallback}
+                        activeCell={this.state.activeCell}
+                        categories={this.state.categories}
+                        searchCategories={this.state.searchCategories}
+                        activeCategories={this.state.activeCategories} />
           </Grid>
           <Grid item>
-            <RightArea callbackFromParent={this.rightAreaCallback} itemCount={this.state.cartItems.length} cartItems={this.state.cartItems} total={this.state.total}/>
+            <RightArea callbackFromParent={this.rightAreaCallback}
+                       itemCount={this.state.cartItems.length}
+                      cartItems={this.state.cartItems}
+                      total={this.state.total}/>
           </Grid>
         </Grid>
       </div>
