f42272a44496196ede6182dd3a3e25dc5c0028e8
Rohan Asmat <m.rohan.a.asmat@gmail.com>
2019-09-17 14:25:54 +0200

    Updated Navbar Component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -20,7 +20,7 @@ const Navbar = props => {
         fontSize: '13px',
         fontFamily: 'Poppins',
         textAlign: 'left',
-        width: '159px',
+        width: '65px',
         fontWeight: '500',
       }
     };
@@ -30,21 +30,20 @@ const Navbar = props => {
             title={<span><span style={{  color: '#CFD5DD'  }}>Rest</span>
                          <span style={{  color: 'black'  }}>raunt</span>
                    </span>}
-            style={{backgroundColor:'white', paddingRight:'0px'}}
-    >
+            style={{backgroundColor:'white', paddingRight:'0px'}}>
     <Toolbar style={{backgroundColor:'#54ad70', width:'24px'}}>
       <Notification style={{color:'white'}} />
     </Toolbar>
-    <Toolbar style={{backgroundColor:'#5bbc79'}}>
+    <Toolbar style={{backgroundColor:'#5bbc79', textAlign:'center'}}>
       <span style={style.profileButton}>
         <span style={style.profileName}>Adam Lane</span>
-        <span style={style.profilePic}>
-        <Avatar alt="Adam Lane" src="../../images/1.png"  />
-        </span>
       </span>
     </Toolbar>
+    <Toolbar style={{backgroundColor:'#5bbc79', width:'30px'}}>
+        <Avatar alt="Adam Lane"
+                style={{backgroundColor:'#fbf7f5', color:'#757575'}}>AL</Avatar>
+    </Toolbar>
     </AppBar>
-
   );
 };
 
